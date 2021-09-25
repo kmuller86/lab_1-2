dc8345232075bcc2a3e938e5662eea6a3524cf2f
przememaj <majorek2509@gmail.com>
2021-09-25 13:19:10 +0200

    cw3

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,53 @@ public class Main {
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
-        System.out.println("Hello");
+        Scanner scanImie = new Scanner(System.in);
+        System.out.print("Podaj imie: ");
+        String imie = scanImie.next();
+
+        Scanner scanNazwisko = new Scanner(System.in);
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scanNazwisko.next();
+
+        Scanner scanWiek = new Scanner(System.in);
+        System.out.print("Podaj wiek: ");
+        String wiek = scanWiek.next();
+
+        Scanner scanNr = new Scanner(System.in);
+        System.out.print("Podaj nr Indeksu: ");
+        String nr = scanNr.next();
+
+        System.out.println(imie+", "+nazwisko+", "+wiek+", "+nr);
+
+        //--------------ZAD 2---------------------------------------------------
+
+
+        Scanner scanliczbaA = new Scanner(System.in);
+        System.out.print("Podaj pierwsza libcze: ");
+        int liczbaA = scanliczbaA.nextInt();
+
+        Scanner scanliczbaB = new Scanner(System.in);
+        System.out.print("Podaj druga libcze: ");
+        int liczbaB = scanliczbaB.nextInt();
+
+        int wynik;
+
+        wynik = liczbaA + liczbaB;
+        System.out.println(wynik);
+
+        wynik = liczbaA - liczbaB;
+        System.out.println(wynik);
+
+        wynik = liczbaA * liczbaB;
+        System.out.println(wynik);
+
+        wynik = liczbaA / liczbaB;
+        System.out.println(wynik);
+
+        wynik = liczbaA % liczbaB;
+        System.out.println(wynik);
+
+
 
     }
 }
