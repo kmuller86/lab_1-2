c80f459d1b6f5cbbd9be0db459e7bd270fc437f7
przememaj <majorek2509@gmail.com>
2021-09-25 12:02:42 +0200

    Zad1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Hello " + "world!");
+        System.out.printf("Hello %s \n", "world");
+        System.out.print("Hello!");
 
 
     }
