2a67bf2a246da3ab4a839bf11680261df6045114
przememaj <majorek2509@gmail.com>
2021-09-25 13:33:36 +0200

    cw3

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,25 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        System.out.println("(a && b) : " + (a && b));
+        System.out.println("(a && c) : " + (a && c));
+
+        System.out.println("!a : " + !a);
+        System.out.println("!b : " + !b);
+
+        int x = 5;
+        int y = 7;
+        int z = 5;
+
+        System.out.println("(x == y) : " + (x == y));
+        System.out.println("(x == z) : " + (x == z));
+
+        System.out.println("(x != y) : " + (x != y));
+        System.out.println("(x != z) : " + (x != z));
+
     }
 }
